7107357717150d70c9570504434466444da53571
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-03-06 22:42:10 +0300

    Init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          YN React Redux Enzume Udemy
+      </div>
+    );
+  }
+}
+
+export default App;
